b7cd32a14b24c414ea5ec8c4d662b2204bd149de
Ben <benbromley.104@gmail.com>
2021-11-01 14:37:51 +0000

    replace vue3 files with vue2 files

--- src/main.js
+++ src/main.js
@@ -1,42 +1,8 @@
-import { createApp } from "vue";
-import { createStore } from "vuex";
-import store from './store/index';
-import App from "./App.vue";
-import "./index.css";
-import router from "./routes.js";
-import "bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
 
-// Create a new store instance.
-const oldStore = createStore({
-  state() {
-    return {
-      cart: {
-        totalPrice: 0,
-        itemCount: 0,
-        items: [],
-      },
-    };
-  },
-  mutations: {
-    // TODO: test this method too  
-    addToCart(state, product) {
-      setTimeout(() => {
-        state.cart.items.push({ ...product, cartId: new Date().valueOf() });
-      }, 20);
-    },
-    // TODO: test this method
-    removeFromCart(state, product) {
-      state.cart.items = state.cart.items.filter((item) => {
-        return item.cartId != product.cartId;
-      });
-    },
-  },
-});
-
-const VueMart = createApp(App)
-// Install the store instance as a plugin
-VueMart.use(store);
-VueMart.use(router);
-VueMart.mount("#app");
-
+new Vue({
+  router,
+  render: (h)=>{h(App)}
+}).$mount('#app')
--- src/views/checkout.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="page">
-    <h1>THIS IS THE CHECKOUT</h1>
-  </div>
-</template>
